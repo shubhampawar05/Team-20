00e20d0ca6069c82d1be3652623f792fd987a6ab
aman verma <amanv3093@gmai.com>
2024-02-10 19:01:16 +0530

    add code editor project

---
+++ Aman verma/Code-Editor/script.js
@@ -0,0 +1,11 @@
+
+
+function fun(){
+    let htmlcode=document.getElementById("htmlcode").value;
+    let csscode=document.getElementById("csscode").value;
+    let jscode=document.getElementById("jscode").value;
+    let output=document.getElementById("output");
+    output.contentDocument.body.innerHTML=htmlcode+"<style>"+csscode+"</style>";
+    output.contentWindow.eval(jscode);
+    
+}
