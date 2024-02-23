b12c0cb756e1e0ae30a48d42ed57d62ef93428de
Shubham Chopde <shubhamchopde090@gmail.com>
2024-02-23 11:48:18 +0530

    first commit

---
+++ interview/index.js
@@ -0,0 +1,39 @@
+//  let arr = [2,10,5,100,1]
+// // output = [100,1]
+
+// arr.sort((a,b)=> b -a)
+// let result = []
+// result.push(arr[0])
+// result.push(arr[arr.length - 1])
+// console.log(result);
+// console.log("hello");
+
+
+
+// url = 'https://jsonplaceholder.typicode.com/posts'
+// let ul = document.querySelector('#dataList');
+
+//  async function getData (){
+//     let response = await fetch(url);
+//     let data = await response.json();
+//     // console.log(data);
+//     data.forEach((value) => {
+//         let li = document.createElement('li')
+//         li.innerText = value.title;
+//         ul.appendChild(li);
+//         // console.log(value.title ); 
+//     })
+// }   
+// getData();
+
+// 3.Given an array of strings, use reduce to create an object that counts the occurrences of each string in the array.
+
+// I/P -> const strings = ["apple", "banana", "apple", "cherry", "banana", "apple"];
+
+// Output: 
+
+// {
+//   "apple": 3,
+//   "banana": 2,
+//   "cherry": 1
+// }
